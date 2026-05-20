4365efd510c229b94ae00924cfb4b671e798e5f1
Bach Le <95017142+giabach1106@users.noreply.github.com>
2026-05-20 13:35:16 +0700

    Add: meeting-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-1.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        if (intervals.size() == 0)
+            return 1;
+        sort(intervals.begin(), intervals.end(), [](auto &a, auto &b) {
+            return a.end < b.end;
+        });
+        int res = 0;
+        for (int i = 0; i < intervals.size (); i++) {
+            if (i - 1 >= 0 and intervals[i - 1].end > intervals[i].start) {
+                res++;
+                intervals.erase(intervals.begin() + i);
+                i--;
+            }
+        }
+        return res + 1;
+    }
+};
