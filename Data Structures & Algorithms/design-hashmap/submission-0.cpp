43abfc22a6da9b781204d04a4b032663e15d40a9
Bach Le <95017142+giabach1106@users.noreply.github.com>
2026-06-15 06:32:07 -0400

    Add: design-hashmap - submission-0

---
+++ Data Structures & Algorithms/design-hashmap/submission-0.cpp
@@ -0,0 +1,31 @@
+class MyHashMap {
+private:
+    unordered_map<int, int> a;
+public:
+    MyHashMap() {
+        
+    }
+    
+    void put(int key, int value) {
+        a[key] = value;
+    }
+    
+    int get(int key) {
+        if (a.count(key)) {
+            return a[key];
+        }
+        else return -1;
+    }
+    
+    void remove(int key) {
+        a.erase(key);
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
