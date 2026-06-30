3e4cd3d726265a6a82ed8d99917b2f91dabd0755
Bach Le <95017142+giabach1106@users.noreply.github.com>
2026-07-01 02:09:59 +0700

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int res = 0;
+        int cur = 0;
+        // 2 3 4 4 5 10 20
+        for (int i = 0; i < nums.size(); i++) {
+            if (i - 1 >= 0) {
+                if (nums[i] - nums[i - 1] == 1) {
+                    cur++;
+                    res = max(res, cur);
+                }
+                else if (nums[i] - nums[i - 1] == 0)
+                    continue;
+                else cur = 1;
+                cout << nums[i] << " " << cur << endl;
+            }
+        }
+        return res + 1;
+    }
+};
