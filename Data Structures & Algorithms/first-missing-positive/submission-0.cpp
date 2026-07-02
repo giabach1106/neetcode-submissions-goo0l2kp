13182a0d63f58efd6afefc58f5af271361318111
Bach Le <95017142+giabach1106@users.noreply.github.com>
2026-07-03 01:06:57 +0700

    Add: first-missing-positive - submission-0

---
+++ Data Structures & Algorithms/first-missing-positive/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int min = INT_MAX, max = INT_MIN;
+        for (auto num : nums) {
+            if (min > num)
+                min = num;
+            if (max < num) 
+                max = num;
+        }
+        unordered_set<int> s(nums.begin(), nums.end());
+        for (auto x : s)
+            cout << x << " ";
+        for (int i = min; i <= max; i++) {
+            if (!s.count(i)) 
+                return i;
+        }
+        return max + 1;
+    }
+};
