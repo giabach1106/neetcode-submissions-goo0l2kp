6a1f604ce8e2d973386a0b640f7c99a3faad5c46
Bach Le <95017142+giabach1106@users.noreply.github.com>
2026-07-15 21:26:04 +0700

    Add: rotate-array - submission-6

---
+++ Data Structures & Algorithms/rotate-array/submission-6.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        vector<int> tmp(nums.begin(), nums.end());
+        if (nums.size() % 2 == 0) {
+            for (int i = 0; i < nums.size(); i++) {
+                nums[i] = tmp[(i + k) % nums.size()];
+            }
+        } 
+        else {
+            for (int i = 0; i < nums.size(); i++) {
+                nums[i] = tmp[(i + k + 1) % nums.size()];
+            }
+        }
+    }
+};
