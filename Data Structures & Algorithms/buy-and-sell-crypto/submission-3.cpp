9beb45e4b731ebf8b9234974124b85fe7337bda4
Bach Le <95017142+giabach1106@users.noreply.github.com>
2026-07-20 15:15:38 +0700

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int res = 0;
+        int l = 0, r = 1;
+        while (r < prices.size()) {
+            int profit = prices[r] - prices[l];
+            if (profit < 0) {
+                l = r;
+                r++;
+            } 
+            else {
+                res = max(res, profit);
+                r++;
+            }
+        }
+        return res;
+    }
+};
