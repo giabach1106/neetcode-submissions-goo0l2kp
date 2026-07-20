f13b511e03ddb169b1d8bb345008596f47fff32b
Bach Le <95017142+giabach1106@users.noreply.github.com>
2026-07-20 22:00:39 +0700

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        unordered_set<int> set1 (s1.begin(), s1.end()), set2; 
+        int l = 0, r = 0;
+        while (r < s2.size()) {
+            set2.insert(s2[r]);
+            if (r - l + 1 > s1.size()) {
+                set2.erase(s2[l]);
+                l++;
+            }
+            if (set1 == set2)   
+                return true;
+            r++;
+        }
+        
+        return false;
+    }
+};
