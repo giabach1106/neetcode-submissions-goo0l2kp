754f759bf9a6a862690877d80699c59bf25eaf74
Bach Le <95017142+giabach1106@users.noreply.github.com>
2026-07-26 19:38:28 +0700

    Add: sqrtx - submission-3

---
+++ Data Structures & Algorithms/sqrtx/submission-3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l = 1, r = x;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            int mul = mid * mid;
+            if (mul == x) {
+                return mid;
+            }
+            else if (mul < x) {
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+        return l - 1;
+    }
+};
